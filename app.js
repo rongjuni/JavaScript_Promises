69cfebef8605fcfc2d42a3d47a819f48f2edae0e
Matthew Seo <rongjuni@hotmail.com>
2022-08-09 11:38:39 -0400

    first push

--- app.js
+++ app.js
@@ -2,3 +2,48 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function watchTurorialCallback(callback, errorCallback) {
+  let userLeft = false;
+  let userWatchingLiveStream = true;
+
+  if (userLeft) {
+    errorCallback({
+      name: "User Left",
+      message: ":(",
+    });
+  } else if (userWatchingLiveStream) {
+    callback("Thumbs up and Subscribe");
+  }
+}
+
+watchTurorialCallback(
+  (message) => {
+    console.log(message);
+  },
+  (error) => {
+    console.log(error.name + " " + error.message);
+  }
+);
+
+let watching = new Promise((resolve, reject) => {
+  let userWatchingLiveStream = true;
+
+  if (userWatchingLiveStream) {
+    resolve("Thumbs up and Subscribe!!!");
+  } else {
+    reject("User Left");
+  }
+});
+
+watching
+  .then((message) => {
+    console.log("good news " + message);
+  })
+  .catch((message) => {
+    console.log("bad news " + message);
+  });
+
+// let practice = new Promise((resolve, reject) =>{
+
+// })
